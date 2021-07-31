07de03691ab02b8c9199bb4d3bcf829e24a47a60
hzc168 <1048224980@qq.com>
2021-07-31 21:03:19 +0800

    实现添加商品，formidable读取form表单，存储base64图片

--- src/components/admin/AddProduct.tsx
+++ src/components/admin/AddProduct.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../core/Layout'
-import { Form, Upload, Button, Input, Select } from 'antd'
+import { Form, Upload, Button, Input, Select, message } from 'antd'
 import { UploadOutlined } from '@ant-design/icons'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCategory } from '../../store/actions/category.actions'
 import { AppState } from '../../store/reducers'
 import { CategoryState } from '../../store/reducers/category.reducer'
+import { RcFile } from 'antd/lib/upload'
+import axios from '_axios@0.21.1@axios'
+import { API } from '../../config'
+import { isAuth } from '../../helpers/auth'
+import { Jwt } from '../../store/models/auth'
 
 const AddProduct = () => {
     const dispatch = useDispatch()
+    const [file, setFile] = useState<RcFile>()
 
     const category = useSelector<AppState, CategoryState>(state => state.category)
 
@@ -16,11 +22,50 @@ const AddProduct = () => {
         dispatch(getCategory())
     }, [])
 
-    return (
-        <Layout title="添加商品" subTitle="">
-            <Form initialValues={{ category: "" }}>
+    const { token } = isAuth() as Jwt
+
+    const onFinish = (product: any) => {
+        const formData = new FormData()
+        console.log(product)
+        for(let attr in product) {
+            formData.set(attr, product[attr])
+        }
+        if(typeof file !== "undefined") {
+            formData.set('photo', file)
+        }
+
+        // const paramData = {
+        //     name: formData.get('name'),
+        //     description: formData.get('description'),
+        //     category: formData.get('category'),
+        //     quantity: formData.get('quantity'),
+        //     shipping: formData.get('shipping'),
+        //     photo: formData.get('photo')
+        // }
+        axios.post(`${API}/reset/product`, formData, {
+        // axios.post(`http://localhost:80/product/create/1234`, formData, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(() => {
+            message.success('商品添加成功')
+        }, () => {
+            message.error('商品添加失败')
+        })
+    }
+
+    const addProductForm = () => {
+        const props = {
+            accept: "image/*",
+            beforeUpload: function (file: RcFile) {
+                setFile(file)
+                return false
+            }
+        }
+        return (
+            <Form initialValues={{ category: "" }} onFinish={onFinish}>
                 <Form.Item>
-                    <Upload>
+                    <Upload {...props}>
                         <Button><UploadOutlined />上传商品封面</Button>
                     </Upload>
                 </Form.Item>
@@ -56,6 +101,12 @@ const AddProduct = () => {
                     <Button type="primary" htmlType="submit">添加商品</Button>
                 </Form.Item>
             </Form>
+        )
+    }
+
+    return (
+        <Layout title="添加商品" subTitle="">
+            {addProductForm()}
         </Layout>
     )
 }
